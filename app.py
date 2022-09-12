7c6f3a465a0821e466bd897a4798637d9e2dd602
yudur <duarteyudi218@gmail.com>
2022-09-11 22:11:25 -0300

    resize plant images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from sprites import index
 
 
 pygame.init()
@@ -7,14 +8,18 @@ size = width, height = 900, 600
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Coin Rush")
+pygame.display.set_icon(index.coin_sprite['coin_0'])
 
-icon = pygame.image.load('sprites/gold.png')
-
-pygame.display.set_icon(icon)
-
+glass_position_width = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    for i in range(len(index.glass_sprites) + 2):
+        if i >= 2:
+            i = 0
+
+        screen.blit(index.glass_sprites[i], (glass_position_width, 550))
+        glass_position_width += 265
 
     pygame.display.flip()
---
+++ sprites/index.py
@@ -0,0 +1,10 @@
+import pygame
+
+coin_sprite = {
+    'coin_0' : pygame.image.load('sprites/coin/coin.png')
+}
+
+glass_sprites = [
+    pygame.image.load('sprites/plant_sprite/tile000.png'),
+    pygame.image.load('sprites/plant_sprite/tile001.png')
+]
