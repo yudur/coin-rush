7aa5140bde0d7a371529c4e8a7dc9641341843fa
yudur <duarteyudi218@gmail.com>
2022-10-01 20:01:47 -0300

    rendering the map infinitely

--- app.py
+++ app.py
@@ -3,23 +3,31 @@ from sprites import index
 
 
 pygame.init()
+clock = pygame.time.Clock()
 
 size = width, height = 900, 600
+black = (0, 0, 0)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Coin Rush")
 pygame.display.set_icon(index.coin_sprite['coin_0'])
 
-glass_position_width = 0
+plant_pos_x, plant_pos_y = 0, 550
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    for i in range(len(index.glass_sprites) + 2):
-        if i >= 2:
-            i = 0
+    screen.fill(black)
 
-        screen.blit(index.glass_sprites[i], (glass_position_width, 550))
-        glass_position_width += 265
+    for i in range(0, 1061, 265):
+        if plant_pos_x == -266:
+            plant_pos_x = 0
 
-    pygame.display.flip()
+        screen.blit(index.plant_sprites[0], (plant_pos_x + i, plant_pos_y))
+
+    pygame.display.update()
+
+    plant_pos_x -= 2
+
+    clock.tick(60)
--- sprites/index.py
+++ sprites/index.py
@@ -4,7 +4,75 @@ coin_sprite = {
     'coin_0' : pygame.image.load('sprites/coin/coin.png')
 }
 
-glass_sprites = [
+plant_sprites = [
     pygame.image.load('sprites/plant_sprite/tile000.png'),
     pygame.image.load('sprites/plant_sprite/tile001.png')
 ]
+
+
+player_sprites = {
+    'idle': [
+        pygame.image.load('sprites/player/Idle__000.png'),
+        pygame.image.load('sprites/player/Idle__001.png'),
+        pygame.image.load('sprites/player/Idle__002.png'),
+        pygame.image.load('sprites/player/Idle__003.png'),
+        pygame.image.load('sprites/player/Idle__004.png'),
+        pygame.image.load('sprites/player/Idle__005.png'),
+        pygame.image.load('sprites/player/Idle__006.png'),
+        pygame.image.load('sprites/player/Idle__007.png'),
+        pygame.image.load('sprites/player/Idle__008.png'),
+        pygame.image.load('sprites/player/Idle__009.png')
+    ],
+
+    'run': [
+        pygame.image.load('sprites/player/Run__000.png'),
+        pygame.image.load('sprites/player/Run__001.png'),
+        pygame.image.load('sprites/player/Run__002.png'),
+        pygame.image.load('sprites/player/Run__003.png'),
+        pygame.image.load('sprites/player/Run__004.png'),
+        pygame.image.load('sprites/player/Run__005.png'),
+        pygame.image.load('sprites/player/Run__006.png'),
+        pygame.image.load('sprites/player/Run__007.png'),
+        pygame.image.load('sprites/player/Run__008.png'),
+        pygame.image.load('sprites/player/Run__009.png')
+    ],
+
+    'slide': [
+        pygame.image.load('sprites/player/Slide__000.png'),
+        pygame.image.load('sprites/player/Slide__001.png'),
+        pygame.image.load('sprites/player/Slide__002.png'),
+        pygame.image.load('sprites/player/Slide__003.png'),
+        pygame.image.load('sprites/player/Slide__004.png'),
+        pygame.image.load('sprites/player/Slide__005.png'),
+        pygame.image.load('sprites/player/Slide__006.png'),
+        pygame.image.load('sprites/player/Slide__007.png'),
+        pygame.image.load('sprites/player/Slide__008.png'),
+        pygame.image.load('sprites/player/Slide__009.png')
+    ],
+
+    'jump': [
+        pygame.image.load('sprites/player/Jump__000.png'),
+        pygame.image.load('sprites/player/Jump__001.png'),
+        pygame.image.load('sprites/player/Jump__002.png'),
+        pygame.image.load('sprites/player/Jump__003.png'),
+        pygame.image.load('sprites/player/Jump__004.png'),
+        pygame.image.load('sprites/player/Jump__005.png'),
+        pygame.image.load('sprites/player/Jump__006.png'),
+        pygame.image.load('sprites/player/Jump__007.png'),
+        pygame.image.load('sprites/player/Jump__008.png'),
+        pygame.image.load('sprites/player/Jump__009.png')
+    ],
+
+    'dead': [
+        pygame.image.load('sprites/player/Dead__000.png'),
+        pygame.image.load('sprites/player/Dead__001.png'),
+        pygame.image.load('sprites/player/Dead__002.png'),
+        pygame.image.load('sprites/player/Dead__003.png'),
+        pygame.image.load('sprites/player/Dead__004.png'),
+        pygame.image.load('sprites/player/Dead__005.png'),
+        pygame.image.load('sprites/player/Dead__006.png'),
+        pygame.image.load('sprites/player/Dead__007.png'),
+        pygame.image.load('sprites/player/Dead__008.png'),
+        pygame.image.load('sprites/player/Dead__009.png')
+    ]
+}
