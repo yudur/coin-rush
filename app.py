04c84191503c59886ec75c63153c25d9db2148d5
yudur <duarteyudi218@gmail.com>
2022-10-02 11:58:49 -0300

    adding player

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pygame, sys
 from sprites import index
 
@@ -14,11 +15,100 @@ pygame.display.set_icon(index.coin_sprite['coin_0'])
 
 plant_pos_x, plant_pos_y = 0, 550
 
+class PlayerSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.acount = 0
+
+        self.animation = index.player_sprites['run']
+        self.image = self.animation[0]
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 0
+        self.rect.y = 400
+
+    def update(self):
+        self.acount += 1
+        if self.acount == len(self.animation):
+            self.acount = 0
+        self.image = self.animation[self.acount]
+
+def collision(position_player: Tuple[int, int], position_object: Tuple[int, int]):
+    if position_player[0] == position_object[0] and position_player[1] == position_object[1]:
+        return True
+    else:
+        return False
+
+group_player = pygame.sprite.Group()
+player_sprite = PlayerSprite()
+group_player.add(player_sprite)
+
+jumpcount = 0
+jumpstate = 0
+
+seconds_counter = 5
+txt=f'{seconds_counter}'                                 
+pygame.font.init()                                
+fonte=pygame.font.get_default_font()             
+fontesys=pygame.font.SysFont(fonte, 60)           
+txttela = fontesys.render(txt, 1, (255,255,255))  
+screen.blit(txttela, (width / 2, height / 2))
+
+move_rigth = 10
+move_left = 10
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                jumpstate = 1
+                player_sprite.animation = index.player_sprites['jump']
+
+    if seconds_counter > 1:
+        screen.fill(black)
+        txt=f'{seconds_counter}'                                          
+        txttela = fontesys.render(txt, 1, (255,255,255))  
+        screen.blit(txttela, (width / 2, height / 2))
+        pygame.display.update()
+
+        seconds_counter -= 1
+        pygame.time.delay(1000)
+        continue
+
+    # Movement functions
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        if not player_sprite.rect.x >= 750:
+            player_sprite.rect.x += move_rigth
+            
+    elif pygame.key.get_pressed()[pygame.K_LEFT]:
+        if not player_sprite.rect.x <= -20:
+            player_sprite.rect.x -= move_left
+
+    if jumpstate:
+        move_rigth = 60
+        move_left = 60
+        jumpcount += 1
+        if jumpcount < 4:
+            player_sprite.rect.y -= 120
+        else:
+            player_sprite.rect.y += 60
+
+        if jumpcount > 8:
+            move_rigth = 10
+            move_left = 10
+            jumpstate = 0
+            jumpcount = 0
+            player_sprite.animation = index.player_sprites['run']
+        
+        pygame.time.delay(20)
+
     screen.fill(black)
+
+    group_player.draw(screen)
+    group_player.update()
 
     for i in range(0, 1061, 265):
         if plant_pos_x == -266:
@@ -30,4 +120,5 @@ while True:
 
     plant_pos_x -= 2
 
-    clock.tick(60)
+    clock.tick(30)
+    
--- sprites/index.py
+++ sprites/index.py
@@ -11,19 +11,6 @@ plant_sprites = [
 
 
 player_sprites = {
-    'idle': [
-        pygame.image.load('sprites/player/Idle__000.png'),
-        pygame.image.load('sprites/player/Idle__001.png'),
-        pygame.image.load('sprites/player/Idle__002.png'),
-        pygame.image.load('sprites/player/Idle__003.png'),
-        pygame.image.load('sprites/player/Idle__004.png'),
-        pygame.image.load('sprites/player/Idle__005.png'),
-        pygame.image.load('sprites/player/Idle__006.png'),
-        pygame.image.load('sprites/player/Idle__007.png'),
-        pygame.image.load('sprites/player/Idle__008.png'),
-        pygame.image.load('sprites/player/Idle__009.png')
-    ],
-
     'run': [
         pygame.image.load('sprites/player/Run__000.png'),
         pygame.image.load('sprites/player/Run__001.png'),
@@ -35,19 +22,6 @@ player_sprites = {
         pygame.image.load('sprites/player/Run__007.png'),
         pygame.image.load('sprites/player/Run__008.png'),
         pygame.image.load('sprites/player/Run__009.png')
-    ],
-
-    'slide': [
-        pygame.image.load('sprites/player/Slide__000.png'),
-        pygame.image.load('sprites/player/Slide__001.png'),
-        pygame.image.load('sprites/player/Slide__002.png'),
-        pygame.image.load('sprites/player/Slide__003.png'),
-        pygame.image.load('sprites/player/Slide__004.png'),
-        pygame.image.load('sprites/player/Slide__005.png'),
-        pygame.image.load('sprites/player/Slide__006.png'),
-        pygame.image.load('sprites/player/Slide__007.png'),
-        pygame.image.load('sprites/player/Slide__008.png'),
-        pygame.image.load('sprites/player/Slide__009.png')
     ],
 
     'jump': [
